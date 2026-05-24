9317657977ec8698af22977b752b6e25e3987f70
Isadora <isf6@aluno.ifnmg.edu.br>
2026-05-24 19:33:15 -0300

    Entrega do modulo Front-end do SaaS

--- core/urls.py
+++ core/urls.py
@@ -14,9 +14,43 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
 
+from core_loja import views
+
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+
+    path('', views.home, name='home'),
+    path(
+        'admin/',
+        admin.site.urls
+    ),
+    path(
+        'admin/',
+        admin.site.urls
+    ),
+
+    path(
+        'loja/<int:empresa_id>/',
+        views.dashboard_loja,
+        name='url_da_loja'
+    ),
+
+    path(
+        'loja/<int:empresa_id>/novo-cliente/',
+        views.cadastrar_item,
+        {'tipo': 'cliente'},
+        name='novo_cliente'
+    ),
+
+    path(
+        'loja/<int:empresa_id>/novo-produto/',
+        views.cadastrar_item,
+        {'tipo': 'produto'},
+        name='novo_produto'
+    ),
+    path('cadastrar-empresa/', views.cadastrar_empresa, name='cadastrar_empresa'),
 ]
